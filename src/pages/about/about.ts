397a75c271f7e60fd4710ec38599b134d306e791
BeerCSIM-AIT <st121032@ait.ac.th>
2020-03-20 23:49:53 +0700

    New project, new pages

---
+++ src/pages/about/about.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AboutPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+})
+export class AboutPage {
+  id: any;
+  name: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.id=this.navParams.get('id');
+    this.name=this.navParams.get('name');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AboutPage');
+  }
+
+}
